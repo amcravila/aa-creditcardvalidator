93a92f421fac1a74323a38a3cfb4b54a55e1d0ab
amcravila <amcravila@gmail.com>
2018-09-05 19:20:57 -0300

    Tests with Throw

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,33 +1,40 @@
 const mocha = require('mocha');
-var assert = require('assert');
+const chai = require('chai');
+const assert = chai.assert;
 const cardValidator = require('../lib/index');
 
 describe('cardValidator()', function() {
-  describe('when there is no parameter', () => {
+  context('when there is no parameter', () => {
     it('should throw an error', () => {
-      assert.equal(cardValidator(''), 'Parâmetro Inválido. Digite um número para validação.');
+      assert.throw(function() {
+        cardValidator();
+      }, 'Parâmetro Inválido. Digite um número de cartão para validação.');
     });
   });
 
-  describe('when number is a string', () => {
+  context('when number is a string', () => {
     it('should throw an error', () => {
-      assert.equal(cardValidator('Texto'), 'Digite apenas números!');
+      assert.throw(function() {
+        cardValidator('Texto');
+      }, 'Digite apenas números!');
     });
   });
 
-  describe('when the number is an integer and there is only one digit', () => {
+  context('when the number is an integer and there is only one digit', () => {
     it('should throw an error', () => {
-      assert.equal(cardValidator(1), 'Parâmetro Inválido. Digite um número para validação.');
+      assert.throw(function() {
+        cardValidator(9);
+      }, 'Digite um número correto de cartão para validação.');
     });
   });
 
-  describe('when the number is an integer and is a valid card', () => {
+  context('when the number is an integer and is a valid card', () => {
     it('should return true', () => {
       assert.equal(cardValidator(36490102462661), true);
     });
   });
 
-  describe('when the card is invalid', () => {
+  context('when the card is invalid', () => {
     it('should return false', () => {
       assert.equal(cardValidator(12345678912345), false);
     });
